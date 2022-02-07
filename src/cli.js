3189283b8bf143d88dd321de7a39f4101a6a1d16
alekseys888 <alekseys888@mail.ru>
2022-02-07 15:50:55 +0400

    ...

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,3 @@
 import readlineSync from 'readline-sync';
-<<<<<<< HEAD
 export const userName = readlineSync.question('May I have your name? ');
-=======
-export cons userName = readlineSync.question('May I have your name? ');
->>>>>>> 17d063c65e35098fa411d26127565b77afef0067
 console.log('Hello, ' + userName + '!');
