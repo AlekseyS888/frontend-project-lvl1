183d6d209198b71b37260d45eb05f3c81c9c5bed
alekseys888 <alekseys888@mail.ru>
2022-02-13 14:57:52 +0400

    step 5

---
+++ bin/brain-even.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+let quantityCorrectAnswer = 0;
+while (quantityCorrectAnswer < 3) {
+  let randomNum = Math.floor(Math.random() * 100);
+  let parityCheckRandomNum = (randomNum % 2 === 0) ? 'yes' : 'no';
+  console.log(`Question: ${randomNum}`);
+  const answer = readlineSync.question('Your answer: ');
+  if (parityCheckRandomNum === answer) {
+  quantityCorrectAnswer += 1;
+  console.log('Correct!');
+  quantityCorrectAnswer === 3 ? console.log(`'Congratulation ${userName}!'`) : '';
+} else if (parityCheckRandomNum != answer) {
+  quantityCorrectAnswer = 4;
+  console.log(`'${answer}' is wrong answer ;(. Correct answer was '${parityCheckRandomNum}'.\nLet's try again, ...!)`);
+} 
+};
