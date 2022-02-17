b277df61bdd567e8f2669ec3ed6c5e93b1902b4d
alekseys888 <alekseys888@mail.ru>
2022-02-17 15:37:46 +0400

    step 5

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,15 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
+console.log('Welcome to the Brain Games!');
+
+const askName = ()=> {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+};
+askName();
+
+
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
 let quantityCorrectAnswer = 0;
@@ -15,6 +24,6 @@ while (quantityCorrectAnswer < 3) {
   quantityCorrectAnswer === 3 ? console.log(`'Congratulation ${userName}!'`) : '';
 } else if (parityCheckRandomNum != answer) {
   quantityCorrectAnswer = 4;
-  console.log(`'${answer}' is wrong answer ;(. Correct answer was '${parityCheckRandomNum}'.\nLet's try again, ...!)`);
+  console.log(`'${answer}' is wrong answer ;(. Correct answer was '${parityCheckRandomNum}'.\nLet's try again, ${userName}!)`);
 } 
 };
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,11 @@
 import readlineSync from 'readline-sync';
 
+export const userName2 = '';
 const askName = () => {
   const userName = readlineSync.question('May I have your name? ');
+  const userName2 = userName;
   console.log(`Hello, ${userName}!`);
-};
+   };
+
+
 export default askName;
