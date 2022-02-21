33da7d3ce9bc2c8f31671394fcee834e4f0e09b6
alekseys888 <alekseys888@mail.ru>
2022-02-21 20:41:32 +0400

    step 6

---
+++ bin/brain-calc.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+const userName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${userName}!`);
+
+console.log('What is the result of the expression?');
+
+let quantityCorrectAnswer = 0;
+while (quantityCorrectAnswer < 3) {
+  let randomNum1 = Math.floor(Math.random() * 100);
+  let randomNum2 = Math.floor(Math.random() * 100);
+  let operator = ['+', '-', '*'];
+  let randomOperator = operator[Math.floor(Math.random() * operator.length)];
+  let correctAnswer = 0;  
+    switch (randomOperator) {
+      case '+':
+        correctAnswer = randomNum1 + randomNum2;
+        break;
+      case '-':
+        correctAnswer = randomNum1 - randomNum2;
+        break;
+      case '*':
+        correctAnswer = randomNum1 * randomNum2;
+        break;
+    }
+
+  console.log(`Question: ${randomNum1}${randomOperator}${randomNum2}`);
+  const answer = readlineSync.question('Your answer: ');
+  if (correctAnswer = answer) {
+  quantityCorrectAnswer += 1;
+  console.log('Correct!');
+  quantityCorrectAnswer = 3 ? console.log(`'Congratulation, ${userName}!'`) : '';
+} else if (correctAnswer != answer) {
+  quantityCorrectAnswer = 4;
+  console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!)`);
+} 
+};
