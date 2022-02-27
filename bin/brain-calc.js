ecac05495b516404a726c9ff80f0bd27cf042421
AlekseyS888 <alekseys888@mail.ru>
2022-02-27 15:43:26 +0400

    fix bugs

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -24,16 +24,18 @@ while (quantityCorrectAnswer < 3) {
     case '*':
       correctAnswer = randomNum1 * randomNum2;
       break;
+    default:
+      console.log('чо-то не так!');
   }
   console.log(`Question: ${randomNum1}${randomOperator}${randomNum2}`);
   const answer = readlineSync.question('Your answer: ');
-  if (correctAnswer === parseInt(answer)) {
+  if (correctAnswer === Number(answer)) {
     quantityCorrectAnswer += 1;
     console.log('Correct!');
     if (quantityCorrectAnswer === 3) {
       console.log(`'Congratulation, ${userName}!'`);
     }
-  } if (correctAnswer !== parseInt(answer)) {
+  } if (correctAnswer !== Number(answer)) {
     quantityCorrectAnswer = 4;
     console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!)`);
   }
