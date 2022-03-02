9bd25a33c0ebce3072474e073b21221c366ba599
AlekseyS888 <alekseys888@mail.ru>
2022-03-02 21:15:10 +0400

    fix step 7

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 
 <a href="https://codeclimate.com/github/codeclimate/codeclimate/maintainability"><img src="https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/maintainability" /></a>
 
-https://asciinema.org/a/W0lUinsY0633QU6FhkrewSilf
+brain-even: https://asciinema.org/a/W0lUinsY0633QU6FhkrewSilf
+Brain-calc:
--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -9,19 +9,19 @@ console.log('Find the greatest common divisor of given numbers.');
 
 let quantityCorrectAnswer = 0;
 while (quantityCorrectAnswer < 3) {
-let randomNum1 = Math.floor(Math.random() * 10);
-let randomNum2 = Math.floor(Math.random() * 10);
+  const randomNum1 = Math.floor(Math.random() * 10);
+  const randomNum2 = Math.floor(Math.random() * 10);
 
-  function correctAnswerGcd (randomNum1, randomNum2) {
+  const correctAnswerGcd = () => {
     if (!randomNum2) {
       return randomNum1;
     }
     return correctAnswerGcd(randomNum2, randomNum1 % randomNum2);
-  }
+  };
 
-let correctAnswer = correctAnswerGcd(randomNum1, randomNum2);
+  const correctAnswer = correctAnswerGcd(randomNum1, randomNum2);
 
-console.log(`Question: ${randomNum1} ${randomNum2}`);
+  console.log(`Question: ${randomNum1} ${randomNum2}`);
   const answer = readlineSync.question('Your answer: ');
   if (correctAnswer === Number(answer)) {
     quantityCorrectAnswer += 1;
@@ -33,4 +33,4 @@ console.log(`Question: ${randomNum1} ${randomNum2}`);
     quantityCorrectAnswer = 4;
     console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!)`);
   }
-};
+}
