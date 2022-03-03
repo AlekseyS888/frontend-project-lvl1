a8f13409cc40e2afeac9035927211585c717f610
AlekseyS888 <alekseys888@mail.ru>
2022-03-03 22:27:08 +0400

    fix step 7

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -7,17 +7,17 @@ console.log(`Hello, ${userName}!`);
 
 console.log('Find the greatest common divisor of given numbers.');
 
+const correctAnswerGcd = (randomNum1, randomNum2) => {
+  if (!randomNum2) {
+    return randomNum1;
+  }
+  return correctAnswerGcd(randomNum2, randomNum1 % randomNum2);
+};
+
 let quantityCorrectAnswer = 0;
 while (quantityCorrectAnswer < 3) {
   const randomNum1 = Math.floor(Math.random() * 10);
   const randomNum2 = Math.floor(Math.random() * 10);
-
-  const correctAnswerGcd = () => {
-    if (!randomNum2) {
-      return randomNum1;
-    }
-    return correctAnswerGcd(randomNum2, randomNum1 % randomNum2);
-  };
 
   const correctAnswer = correctAnswerGcd(randomNum1, randomNum2);
 
